4c4f1f7c05af42c7a05ff70a7bcb3e1a8c9cae48
JayKalambate <139536749+JayKalambate@users.noreply.github.com>
2025-01-03 17:56:24 +0530

    This is program for Relational Operators

---
+++ Relational_Operators.java
@@ -0,0 +1,33 @@
+public class Relational_Operators {
+    public static void main(String[]args)
+    {
+        int a = 47;
+        int b= 23;
+
+        // less than
+        boolean Less = a<b;
+        System.out.println(Less);
+
+        // Greater Than
+        boolean Greater = a>b;
+        System.out.println(Greater);
+
+        // equals too
+        boolean equ = a==b;
+        System.out.println(equ);
+
+        //less than equals to
+        boolean Lessequ = a<=b;
+        System.out.println(Lessequ);
+
+        // Greater than equals to
+        boolean Greequ = a>=b;
+        System.out.println(Greequ);
+
+        // Not equals to
+        boolean Not = a!=b;
+        System.out.println(Not);
+
+    }
+    
+}
