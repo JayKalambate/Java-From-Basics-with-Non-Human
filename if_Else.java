244a9ae033cb0002a90a0ae70dd967b213115fad
JayKalambate <139536749+JayKalambate@users.noreply.github.com>
2025-01-03 17:57:45 +0530

    This is An program to check Even or Odd

---
+++ if_Else.java
@@ -0,0 +1,15 @@
+public class if_Else {
+    public static void main(String[] args) {
+        int a = 10;
+        int result = 0;
+        if (a%2==0) 
+        {
+            System.out.println("This is an even number");
+        }
+        else{
+            System.out.println("This is an odd number");
+        }
+    }
+    
+    
+}
