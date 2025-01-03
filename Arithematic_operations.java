2b7709fa2986008d8eeaca38574723eed7207428
JayKalambate <139536749+JayKalambate@users.noreply.github.com>
2025-01-03 17:54:42 +0530

    This is Arithematic Operations

---
+++ Arithematic_operations.java
@@ -0,0 +1,27 @@
+public class Arithematic_operations {
+    public static void main(String[] args) {
+        int a = 25;
+        int b =120;
+
+        // Addition 
+        int result1 = a+b;
+        System.out.println(result1);
+
+        //Subtract
+        int sub = a-b;
+        System.out.println(sub);
+
+        //Multiplication
+        int multi = a*b;
+        System.out.println(multi);
+
+        // Division
+        int Div = a/b;
+        System.out.println(Div);
+
+        // Module
+        int mod = a%b;
+        System.out.println(mod);
+    }
+    
+}
