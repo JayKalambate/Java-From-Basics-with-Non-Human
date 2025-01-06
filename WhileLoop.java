74e08237ee2d6da555339be8ba3fdcaabf888f23
JayKalambate <139536749+JayKalambate@users.noreply.github.com>
2025-01-06 16:29:46 +0530

    While loop

---
+++ WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i<12)
+        {
+            System.out.println("Aliens Don't have Brains"+i);
+            i++;
+        }
+    }
+    
+}
