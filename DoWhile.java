69816028a66bab31fa8c330fe8c1c0e26478d41e
JayKalambate <139536749+JayKalambate@users.noreply.github.com>
2025-01-06 16:36:06 +0530

    Do While Loop

---
+++ DoWhile.java
@@ -0,0 +1,15 @@
+public class DoWhile {
+    public static void main(String[]args)
+    {
+        int i =1;
+        do
+        {
+            System.out.println("Aliens Don't have Brains"+i);
+            i++;
+        }
+        while(i<12);
+        System.out.println("Aliens have eyes to watch you");
+
+    }
+    
+}
