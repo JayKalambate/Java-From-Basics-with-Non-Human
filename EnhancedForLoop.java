001ea75dc1c909fa6a98ab9af76c292b227ae366
JayKalambate <139536749+JayKalambate@users.noreply.github.com>
2025-01-11 18:24:32 +0530

    This is an Enhanced Forloop

---
+++ EnhancedForLoop.java
@@ -0,0 +1,14 @@
+public class EnhancedForLoop {
+    public static void main(String args[]){
+        int arr[] = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+
+        for(int i:arr)      // Enhanced for loop
+        {
+            System.out.println(i);
+        }
+    }
+    
+}
