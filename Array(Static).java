5b50a3825215e112c58f4ba471c176139302bfb1
JayKalambate <139536749+JayKalambate@users.noreply.github.com>
2025-01-11 17:51:28 +0530

    This is Program to Learn Static Array

---
+++ Array(Static).java
@@ -0,0 +1,10 @@
+class ArrayStatic {
+    public static void main(String args[]){
+        int arr[] ={23,34,45,56,67,78,89,90}; // This is a static array
+
+        for(int i =0; i<arr.length; i++){  // This for loop is used to print the array elements
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
